e13572d513cd3c6eb07f730afe4280574cfe781d
Frost <matthias.prost@epitech.eu>
2018-07-01 19:47:45 +0200

    Configured basic auth page

--- src/pages/auth/login/login.ts
+++ src/pages/auth/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-login',
@@ -7,11 +7,30 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  email: string = null;
+  password: string = null;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
     //
+  }
+
+  login() {
+    if (!this.email || !this.password) {
+      let message: Array<string> = [];
+
+      this.email ? null: message.push('Error: email is empty');
+      this.password ? null : message.push('Error: password is empty');
+
+      const toast = this.toastCtrl.create({
+        message: message[0],
+        duration: 3000,
+        position: 'top'
+      });
+      toast.present();
+    }
   }
 
 }
