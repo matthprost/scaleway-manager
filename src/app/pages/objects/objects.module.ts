ac3f30d5f3bfcf4782d4c40c44fb942b33c95a8b
Frost <matthias.prost@epitech.eu>
2020-03-25 17:15:48 +0900

    Added bucket list view

--- src/app/pages/objects/objects.module.ts
+++ src/app/pages/objects/objects.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { ObjectsPage } from './objects.page';
+import {ComponentsModule} from '../../components/components.module';
 
 const routes: Routes = [
   {
@@ -15,12 +16,13 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule.forChild(routes),
+        ComponentsModule
+    ],
   declarations: [ObjectsPage]
 })
 export class ObjectsPageModule {}
--- src/app/pages/objects/objects.page.ts
+++ src/app/pages/objects/objects.page.ts
@@ -9,6 +9,10 @@ import {StatusBar} from '@ionic-native/status-bar/ngx';
 })
 export class ObjectsPage implements OnInit {
 
+  public bucketsPar = null;
+  public bucketsAms = null;
+  public isLoading = true;
+
   constructor(private objectService: ObjectService, private statusBar: StatusBar) {
     this.statusBar.styleDefault();
   }
@@ -17,9 +21,13 @@ export class ObjectsPage implements OnInit {
   }
 
   async ionViewDidEnter() {
+    this.isLoading = true;
     this.statusBar.styleDefault();
 
     const result = await this.objectService.getAllBuckets();
+    this.bucketsPar = result.s3par;
+    this.bucketsAms = result.s3ams;
+    this.isLoading = false;
     console.log('RESULT:', result);
   }
 }
