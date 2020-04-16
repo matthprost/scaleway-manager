304ba3b84c4580ec3f87c806a4acfc9bfc16e291
Frost <matthias.prost@epitech.eu>
2020-04-16 13:59:08 +0200

    Added alert message confirmation for deleting object

--- src/app/pages/buckets/objects/options/options.page.ts
+++ src/app/pages/buckets/objects/options/options.page.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {LoadingController, NavParams, PopoverController} from '@ionic/angular';
+import {AlertController, LoadingController, NavParams, PopoverController} from '@ionic/angular';
 import {ObjectService} from '../../../../services/object/object.service';
 
 @Component({
@@ -15,7 +15,7 @@ export class OptionsPage implements OnInit {
   public bucket: string = null;
 
   constructor(private navParams: NavParams, private objectService: ObjectService, private loadingCtrl: LoadingController,
-              private popoverController: PopoverController) {
+              private popoverController: PopoverController, private alertCtrl: AlertController) {
     this.type = this.navParams.get('type');
     this.object = this.navParams.get('object');
     this.region = this.navParams.get('region');
@@ -26,15 +26,32 @@ export class OptionsPage implements OnInit {
   }
 
   public async deleteObject() {
-    const loading = await this.loadingCtrl.create({
-      message: 'Loading...',
-      mode: 'ios'
+    const alert = await this.alertCtrl.create({
+      header: 'Delete File',
+      message: 'This will permanently delete your file. This action is irreversible.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        }, {
+          text: 'Delete',
+          cssClass: 'danger',
+          handler: async (values) => {
+            const loading = await this.loadingCtrl.create({
+              message: 'Loading...',
+              mode: 'ios'
+            });
+
+            await loading.present();
+            await this.objectService.deleteObject(this.bucket, this.region, this.object.Key ? this.object.Key[0] : this.object.Prefix[0]);
+            await this.popoverController.dismiss({reload: true});
+            await loading.dismiss();
+          }
+        }
+      ]
     });
 
-    await loading.present();
-    await this.objectService.deleteObject(this.bucket, this.region, this.object.Key ? this.object.Key[0] : this.object.Prefix[0]);
-    await this.popoverController.dismiss({ reload: true });
-    await loading.dismiss();
+    await alert.present();
 
     return;
   }
