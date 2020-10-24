9a16c7311b760367119779e1eaace314aa3455c4
Matthias <matthias.prost@epitech.eu>
2020-10-25 00:59:45 +0200

    fix: removed useless cordova plugins

--- src/app/pages/products/buckets/objects/objects.module.ts
+++ src/app/pages/products/buckets/objects/objects.module.ts
@@ -9,10 +9,8 @@ import { ObjectsPage } from './objects.page';
 import {ComponentsModule} from '../../../../components/components.module';
 import {OptionsPage} from './options/options.page';
 import {PipesModule} from '../../../../pipes/pipes.module';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {ObjInfosPage} from './options/obj-infos/obj-infos.page';
-import {FileTransfer, FileTransferObject} from '@ionic-native/file-transfer/ngx';
-import { File } from '@ionic-native/file/ngx';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 const routes: Routes = [
   {
@@ -33,10 +31,5 @@ const routes: Routes = [
   ],
   declarations: [ObjectsPage, OptionsPage, ObjInfosPage],
   entryComponents: [OptionsPage, ObjInfosPage],
-  providers: [
-    FileTransferObject,
-    FileTransfer,
-    File
-  ]
 })
 export class ObjectsPageModule {}
